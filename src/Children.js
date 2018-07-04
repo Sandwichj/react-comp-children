7ac6668c05881261acf8c3da9275de20bce65936
Sandwichj <dreamingsandwich@gmail.com>
2018-07-04 11:30:55 +0900

    separated environments as dev and prod. And added a plugin to remove prop-types for production

---
+++ src/Children.js
@@ -0,0 +1,54 @@
+import { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+class Children extends PureComponent {
+  getActiveItems(items, active) {
+    const activeItems = [];
+
+    if (!items) {
+      return null;
+    }
+
+    if ((typeof active === 'undefined') || active === null) {
+      return Array.isArray(items) ? items : Object.keys(items).map(key => items[key]);
+    }
+
+    if (!Array.isArray(active)) {
+      return items[active];
+    }
+
+    if (Array.isArray(items) && Array.isArray(active)) {
+      active.sort();
+    }
+
+    active.forEach(activeItemIndex => {
+      activeItems.push(items[activeItemIndex]);
+    });
+
+    return activeItems;
+  }
+
+  render() {
+    const items = this.props.items || this.props.children;
+    const active = this.props.active;
+
+    return getActiveItems(items, active);
+  }
+}
+
+Children.propTypes = {
+  items: PropTypes.oneOfType([
+    PropTypes.array,
+    PropTypes.object,
+  ]),
+  active: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.string,
+    PropTypes.arrayOf(PropTypes.oneOfType([
+      PropTypes.number,
+      PropTypes.string,
+    ])),
+  ]),
+};
+
+export default Children;
