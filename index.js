6a61ce40e8b73df970ac8f2cec3928f1e8361a10
Sandwichj <dreamingsandwich@gmail.com>
2018-07-04 22:47:57 +0900

    fix wrong production file path.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./build/react-comp-children.production.js');
+  module.exports = require('./build/react-comp-children.production.min.js');
 } else {
-  module.exports = require('./build/react-comp-children.development.min.js');
+  module.exports = require('./build/react-comp-children.development.js');
 }
